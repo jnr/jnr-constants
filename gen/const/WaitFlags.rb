b1a99a2bb7e90119c62c0d766433e579ddf0c7be
Wayne Meissner <wmeissner@gmail.com>
2009-01-23 18:50:32 +1000

    Add signals and wait flags

---
+++ gen/const/WaitFlags.rb
@@ -0,0 +1,14 @@
+require 'gen/ConstGenerator'
+def gen_waitflags_java(options)
+  ConstGenerator.new 'platform.waitflags', options do |cg|
+    cg.include "sys/wait.h"
+    %w[
+WNOHANG
+WUNTRACED
+WSTOPPED
+WEXITED
+WCONTINUED
+WNOWAIT
+    ].each {|c| cg.const(c, "%#x", "(unsigned int)") { |v| v.hex} }
+  end
+end
