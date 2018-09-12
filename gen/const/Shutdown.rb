5f48a6c86df35e2bde5c88817ac086f8bb579fcd
Charles Oliver Nutter <headius@headius.com>
2018-09-11 20:15:26 -0500

    Set min_value for SHUT_* constants to 0.

--- gen/const/Shutdown.rb
+++ gen/const/Shutdown.rb
@@ -2,6 +2,7 @@ require_relative '../../gen/ConstGenerator'
 def gen_shutdown_java(options)
   ConstGenerator.new 'platform.shutdown', options do |cg|
     cg.include "sys/socket.h"
+    cg.min_value = 0
     %w[
       SHUT_RD
       SHUT_WR
